a4df35f63c6355457cf6b4af32eb3536c725238d
Manu <manojc6363530865@gmail.com>
2025-05-19 22:47:30 +0530

    new movie exe in backend

---
+++ moviapp/src/app/movies/db.ts
@@ -0,0 +1,26 @@
+
+export const movies = [{
+    id: 1,
+    title: "The Matrix",
+    year: 1999
+},
+{
+    id: 2,
+    title: "The Matrix Revolutions",
+    year: 2003
+},
+{
+    id: 3,
+    title: "The Matrix Reloaded",
+    year: 2003
+},
+{
+    id: 4,
+    title: "The Matrix Resurrections",
+    year: 2021
+},
+{
+    id: 5,
+    title: "The Matrix manu",
+    year: 2021
+}]
---
+++ moviapp/src/app/movies/route.ts
@@ -0,0 +1,15 @@
+
+import { movies } from "./db";
+
+export  async function GET() {
+    return  Response.json(movies);
+}
+
+//post method
+
+export async function POST(request: Request) {
+    let movie = await request.json();
+    const newmovie = {...movie}
+    movies.push(newmovie);
+    return new Response(JSON.stringify(newmovie));
+}
